e5a3140aadffe35a784fca93a082a5191ee0850f
CoyotWilly <coyotwilly@gmail.com>
2023-03-16 13:20:10 +0100

    Initial commit

---
+++ Nomad/Nomad/src/main/java/com/coyotwilly/nomad/Nomad/model/User.java
@@ -0,0 +1,8 @@
+package com.coyotwilly.nomad.Nomad.model;
+
+public class User {
+    private Long id;
+    private String firstName;
+    private String lastName;
+
+}
---
+++ Nomad/Nomad/src/test/java/com/coyotwilly/nomad/Nomad/NomadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coyotwilly.nomad.Nomad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NomadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
